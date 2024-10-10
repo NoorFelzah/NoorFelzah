fdbc252b6374dcb98cb7cbc78facf23f311f1c0e
NoorFelzah <cahayafelzah@gmail.com>
2024-10-10 15:52:15 +0800

    17_Sysmbol

---
+++ bin/Tugas1_Noorfelzah/17_Sysmbol.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Inisialisasi Symbol
+  Symbol symbol1 = Symbol('Noor Felzah');
+  var symbol2 = #belajar;
+
+  // Mencetak nilai Symbol
+  print('Symbol 1: $symbol1');
+  print('Symbol 2: $symbol2');
+}
