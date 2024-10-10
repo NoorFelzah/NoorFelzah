dc072fed9860b622507579aeaa88a27a115bc5ef
NoorFelzah <cahayafelzah@gmail.com>
2024-10-10 15:46:05 +0800

    9_Operator_Aritmatika

---
+++ bin/Tugas1_Noorfelzah/9_Operator_Aritmatika.dart
@@ -0,0 +1,20 @@
+void main() {
+  var result1 = 10 + 10;
+  var result2 = 100 - 100;
+  var result3 = -5;
+  var result4 = 5;
+  var result5 = 5 * 5;
+  var result6 = 10 / 3;
+  var result7 = 10 ~/ 3;
+  var result8 = 10 % 3;
+
+  // Menampilkan hasil
+  print('result1: $result1');
+  print('result2: $result2');
+  print('result3: $result3');
+  print('result4: $result4');
+  print('result5: $result5');
+  print('result6: $result6');
+  print('result7: $result7');
+  print('result8: $result8');
+}
