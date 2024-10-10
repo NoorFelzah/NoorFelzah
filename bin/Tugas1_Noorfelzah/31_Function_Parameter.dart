cbeb76b554d4cc71e343537f9d6613d5ca42e160
NoorFelzah <cahayafelzah@gmail.com>
2024-10-10 16:01:26 +0800

    31_Function_Parameter

---
+++ bin/Tugas1_Noorfelzah/31_Function_Parameter.dart
@@ -0,0 +1,8 @@
+void main() {
+  SayHello('Noor', 'Felzah');
+  SayHello('NoorFelzah', 'NoorFelzah');
+}
+
+void SayHello(String firstName, String lastName) {
+  print('Hello $firstName $lastName');
+}
