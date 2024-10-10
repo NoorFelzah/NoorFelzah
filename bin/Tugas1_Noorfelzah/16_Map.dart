25cd49d210a34a144ac042ccfc4487a2cc1b4593
NoorFelzah <cahayafelzah@gmail.com>
2024-10-10 15:51:43 +0800

    16_Map

---
+++ bin/Tugas1_Noorfelzah/16_Map.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Inisialisasi Map untuk Dart
+  Map<String, String> person = {};
+  Map<String, String> product = {};
+  Map<String, String> address = {};
+
+  // Mencetak isi dari masing-masing Map
+  print("Person Map: $person");
+  print("Product Map: $product");
+  print("Address Map: $address");
+}
