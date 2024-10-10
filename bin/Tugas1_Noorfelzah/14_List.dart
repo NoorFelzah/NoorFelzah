2d2f4d55e27c1010c734be5b42605e11ba7c9003
NoorFelzah <cahayafelzah@gmail.com>
2024-10-10 15:50:40 +0800

    14_List

---
+++ bin/Tugas1_Noorfelzah/14_List.dart
@@ -0,0 +1,18 @@
+void main() {
+  // Create List of int
+  List<int> intList = [];
+
+  // Create List of String
+  List<String> stringList = [];
+
+  // Contoh penambahan elemen
+  intList.add(1);
+  intList.add(2);
+
+  stringList.add('Hello');
+  stringList.add('Felzah');
+
+  // Menampilkan isi List
+  print('List of int: $intList');
+  print('List of String: $stringList');
+}
