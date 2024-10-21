7cc7c9033e0123df9926e6ed625d0f5b0a8eae4c
NoorFelzah <cahayafelzah@gmail.com>
2024-10-21 10:40:07 +0800

    18_Inheritance

---
+++ bin/Tugas2_Noorfelzah/18_Inheritance.dart
@@ -0,0 +1,14 @@
+class Manajer {
+  String? nama;
+  void sapa(String nama) {
+    print('Halo $nama, nama saya ${this.nama}');
+  }
+}
+
+class WakilPresiden extends Manajer {}
+
+void main() {
+  var wakilPresiden = WakilPresiden();
+  wakilPresiden.nama = "Noor Felzah";
+  wakilPresiden.sapa("Nuhol");
+}
