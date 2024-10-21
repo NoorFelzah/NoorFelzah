68877d741df4c067409274402d54faa1b2dd2d23
NoorFelzah <cahayafelzah@gmail.com>
2024-10-21 09:35:13 +0800

    9_Variable_Shadowing

---
+++ bin/Tugas2_Noorfelzah/9_Variable_Shadowing.dart
@@ -0,0 +1,13 @@
+void main() {
+  var orang = Orang("Felzah", "Banjarmasin");
+  print(
+      "Nama: ${orang.nama}, Alamat: ${orang.alamat}, Negara: ${orang.negara}");
+}
+
+class Orang {
+  String nama;
+  String? alamat;
+  final String negara = "Indonesia";
+
+  Orang(this.nama, this.alamat);
+}
